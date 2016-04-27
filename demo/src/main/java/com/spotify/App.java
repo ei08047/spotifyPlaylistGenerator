8bf1f875b4cdc943d67522c46ec34812af0ef694
Ze <josemcf89@gmail.com>
2016-04-27 14:42:00 +0100

    project setup

---
+++ demo/src/main/java/com/spotify/App.java
@@ -0,0 +1,18 @@
+package com.spotify;
+import com.spotify.apollo.*;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+
+}
---
+++ minimal/README.txt
@@ -0,0 +1,10 @@
+https://github.com/spotify/apollo/tree/master/apollo-http-service
+
+Compile and Run
+mvn package
+java -jar target/ping.jar
+
+
+Try a request with  curl
+$ curl http://localhost:8080/ping
+pong
